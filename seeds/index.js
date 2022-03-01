c4037e7bf3fac4050ee00d118dc9251c69aee691
MA15 <holladay15@yahoo.com>
2022-03-01 16:57:03 -0500

    routes update

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/connection');
 const seedGallery = require('./galleryData');
 const seedMovie = require('./movieData');
-const seedPaintings = require('./paintingData');
+
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   await seedGallery();
